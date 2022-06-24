7b77e06d2e5e1d4c1260888b14639fc22ff722f6
MLivanos <prodigiousmike@gmail.com>
2022-06-23 19:22:21 -0700

    Add: Ability to Generate Rooms Randomly Rather Than Scripted

--- My project/Assets/Scripts/Enviroment/DungeonRoom.cs
+++ My project/Assets/Scripts/Enviroment/DungeonRoom.cs
@@ -51,30 +51,14 @@ public class DungeonRoom : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        setRoomSize(5,5);
+        setRandomRoomSize();
         dungeonWalls = new GameObject[4];
-        GameObject wall = (GameObject)Instantiate(wallPrefab);
-        DungeonWall Dwall = wall.GetComponent<DungeonWall>();
-        Dwall.hasDoor = true;
-        Dwall.sizeX = sizeX * scale;
-        Dwall.sizeY = 5;
-        Dwall.sizeZ = 1;
-        Dwall.addDoor(40, transform.position);
-        dungeonWalls[0] = wall;
-        for (int i = 1; i < 4; i++)
-        {
-            dungeonWalls[i] = (GameObject)Instantiate(wallPrefab);
-            dungeonWalls[i].GetComponent<DungeonWall>().sizeX = sizeX * scale;
-            dungeonWalls[i].GetComponent<DungeonWall>().sizeY = 5;
-            dungeonWalls[i].GetComponent<DungeonWall>().sizeZ = 1;
-            dungeonWalls[i].GetComponent<DungeonWall>().noDoor(transform.position);
-        }
-        dungeonWalls[0].transform.Translate(25, 0, 0);
-        dungeonWalls[1].transform.Translate(-25, 0, 0);
-        dungeonWalls[2].transform.Translate(0, 0, 25);
-        dungeonWalls[3].transform.Translate(0, 0, -25);
-        dungeonWalls[0].transform.Rotate(Vector3.up, 90);
-        dungeonWalls[1].transform.Rotate(Vector3.up, 90);
+        dungeonWalls[0] = addDooredWall(sizeX, 5, Random.Range(5,scale * sizeX - 5));
+        dungeonWalls[1] = addWall(sizeX, 5);
+        dungeonWalls[2] = addWall(sizeZ, 5);
+        dungeonWalls[3] = addWall(sizeZ, 5);
+        placeWalls(dungeonWalls);
+        assignChildren(dungeonWalls);
     }
 
     // Update is called once per frame
@@ -95,5 +79,46 @@ public class DungeonRoom : MonoBehaviour
         sizeX = x;
         sizeZ = z;
         transform.localScale = new Vector3(sizeX, 1, sizeZ);
+    }
+
+    GameObject addDooredWall(int wallLength, int wallHeight, int doorPos)
+    {
+        GameObject wall = (GameObject)Instantiate(wallPrefab);
+        DungeonWall Dwall = wall.GetComponent<DungeonWall>();
+        Dwall.hasDoor = true;
+        Dwall.sizeX = wallLength * scale;
+        Dwall.sizeY = wallHeight;
+        Dwall.sizeZ = 1;
+        Dwall.addDoor(doorPos, transform.position);
+        return wall;
+    }
+
+    GameObject addWall(int wallLength, int wallHeight)
+    {
+        GameObject wall = (GameObject)Instantiate(wallPrefab);
+        DungeonWall Dwall = wall.GetComponent<DungeonWall>();
+        Dwall.sizeX = wallLength * scale;
+        Dwall.sizeY = wallHeight;
+        Dwall.sizeZ = 1;
+        Dwall.noDoor(transform.position);
+        return wall;
+    }
+
+    void placeWalls(GameObject[] walls)
+    {
+        walls[3].transform.Translate(sizeX * scale / 2, 0, 0);
+        walls[2].transform.Translate(-1 * sizeX * scale / 2, 0, 0);
+        walls[1].transform.Translate(0, 0, sizeZ * scale / 2);
+        walls[0].transform.Translate(0, 0, -1 * sizeZ * scale / 2);
+        walls[3].transform.Rotate(Vector3.up, 90);
+        walls[2].transform.Rotate(Vector3.up, 90);
+    }
+
+    void assignChildren(GameObject[] walls)
+    {
+        for(int i = 0; i < walls.Length; i++)
+        {
+            walls[i].transform.parent = gameObject.transform;
+        }
     }
 }
